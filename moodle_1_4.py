6dfe59c387db5b02f39fba2670f28411c78d0ca5
ilyalom55 <lom_ilya@bk.ru>
2024-12-12 14:23:36 +0300

    moodle_1_4

---
+++ moodle_1_4.py
@@ -0,0 +1,9 @@
+my_string = int(input('Сколько Птс?'))
+dota_pts_max = 15000
+current_year = 2024
+my_string = input('в каком году начали играть?')
+print('в каком году начали играть?'.upper())
+print('в каком году начал играть?'.lower())
+my_string = 'в каком году начали играть?'
+print(my_string [0])
+print((my_string[-1]))

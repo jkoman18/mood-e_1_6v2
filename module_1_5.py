d296afcf06a967f30c8993d8cf3ddaac424e368d
ilyalom55 <lom_ilya@bk.ru>
2024-12-12 14:25:34 +0300

    moodle_1_5

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = 2 , 'banana' , True
+print(immutable_var)
+#immutable_var[1] = 'peach' кортеж не изменяется
+#print(immutable_var)
+mutable_list = ['food', 'peach', 'banana']
+print(mutable_list)
+mutable_list[0] = 'apple'
+mutable_list[1] = 'mango'
+mutable_list[2] = 'cheese'
+print(mutable_list)

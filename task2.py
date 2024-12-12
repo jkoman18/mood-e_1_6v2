b09df21a7e1be23ee2b98109ad0e1eb1a12545ba
ilyalom55 <lom_ilya@bk.ru>
2024-12-12 14:18:53 +0300

    moodle_1_3

---
+++ task2.py
@@ -0,0 +1,9 @@
+home_work = 12
+print(home_work)
+numbers_of_hors = 1.5
+print(numbers_of_hors)
+curs_name = "Python"
+print(curs_name)
+time_one_task = (1.5 / 12)
+print(time_one_task)
+print('Курс:' , curs_name, 'всего задач:' ,home_work , 'затрачено часов:', numbers_of_hors, 'среднее время выполнения', time_one_task,'часа')
